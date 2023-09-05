235ade90f736a4328de1e7a3cce86a26ec54413f
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-09-05 15:06:42 +0600

    list_date_5

--- utils.py
+++ utils.py
@@ -6,4 +6,15 @@ def operations_list():
     f.close()
     return content
 
-print(operations_list()[0])
+def list_date_5():
+    '''Создает список дат 5 последних операций'''
+    list_date = []
+    for operation in operations_list():
+        if operation:
+            list_date.append(operation['date'])
+
+    list_date.sort(reverse=True)
+    list_date_5 = list_date[:5]
+    return list_date_5
+
+print(list_date_5())
